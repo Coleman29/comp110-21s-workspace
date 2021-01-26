9885d1640d6dad67895f9a6dafccf5a5667473e2
Coleman29 <csalloum@live.unc.edu>
2021-01-26 16:54:19 -0500

    Practice for diagram in class

---
+++ lessons/input.py
@@ -0,0 +1,8 @@
+"""A program to demonsrate user input and variables"""
+
+name: str=input("Who are you? ")
+
+print("Wow, " + name + ", you rock!")
+print( name + " have a great day!")
+#The code follows parenthesis so it asks who you are which becomes your name. then it uses your name as the input, and replaces the name with a variable. 
+#Declared a string name as a variable
