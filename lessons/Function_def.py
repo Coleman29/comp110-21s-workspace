cacbfdf4ffc928a7ab972904c799e94086bf2820
Coleman29 <csalloum@live.unc.edu>
2021-02-06 10:54:58 -0500

    Function Definition

---
+++ lessons/Function_def.py
@@ -0,0 +1,14 @@
+"""" an example function definition"""
+
+def my_max(a: int, b: int) -> int:
+    """Returns the largest argument"""
+    if a >= b:
+        return a
+    else:
+        return b
+X: int=6
+Y: int = 5+2
+Z: int: my_max(x,y)
+Print (z)
+
+print(my_max(10+1,10))
