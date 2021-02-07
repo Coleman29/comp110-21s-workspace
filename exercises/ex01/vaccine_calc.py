e982e63232f6802a12192b1b34499dcde407093d
Coleman29 <csalloum@live.unc.edu>
2021-02-07 18:57:28 -0500

    Finished All of Excercise 01

--- exercises/ex01/vaccine_calc.py
+++ exercises/ex01/vaccine_calc.py
@@ -17,13 +17,13 @@ from datetime import datetime
 from datetime import timedelta
 
 
-Population = int(input("Population: "))
-Doses_administered= int(input("Dose administered: "))
-Doses_per_day= int(input("Doses per day: "))
-Target_percent_vaccinated= int(input("Target percent vaccinated: "))
+Population: int = int(input("Population: "))sss
+Doses_administered: int = int(input("Dose administered: "))
+Doses_per_day: int = int(input("Doses per day: "))
+Target_percent_vaccinated: int = int(input("Target percent vaccinated: "))
 function: int= round((((Target_percent_vaccinated*(Population*2))/100) - Doses_administered)/Doses_per_day)
-vd: timedelta= timedelta(function)
+vaccination_daysleft: timedelta= timedelta(function)
 today: datetime = datetime.today()
-Days= today + vd
-string_days: str= Days.strftime("%B %d, %Y")
-print( "We will reach " + str(Target_percent_vaccinated) + "% vaccination in " + str(function) + " days, which falls on " + string_days)
+Vaccination_End_Days: datetime = today + vaccination_daysleft
+Finish_date: str= Vaccination_End_Days.strftime("%B %d, %Y")
+print( "We will reach " + str(Target_percent_vaccinated) + "% vaccination in " + str(function) + " days, which falls on " + Finish_date)
